6306ebe45d2e88a3aafb0fa785deaa4f5153ad7b
gasti fernandez <gastyfp@gmail.com>
2023-04-11 09:35:40 +0200

    redux boiler

---
+++ src/redux/actions/index.js
@@ -0,0 +1,21 @@
+export const ADD_TODO = "ADD_TODO"
+export const GET_TODO = "GET_TODO"
+export const CHECK_TODO = "CHECK_TODO"
+export const DELETE_TODO = "DELETE_TODO"
+
+
+export const getTodo = () => {
+
+}
+
+export const addTodo = () =>{
+
+}
+
+export const checkTodo = () =>{
+
+}
+
+export const deleteTodo = () =>{
+
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,13 @@
+import {GET_TODO,ADD_TODO,CHECK_TODO,DELETE_TODO} from "../actions/index"
+
+const initialState = {
+    todos: [],
+}
+
+export default function reducer(state = initialState , action){
+    switch(action){
+
+        default: return state
+    }
+}
+
